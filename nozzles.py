a247fb05c7bb0c8386cd2f770adb888868edbcae
Julian Raaf <57473270+Foodisgood4@users.noreply.github.com>
2025-07-16 18:04:19 +0100

    Update nozzles.py
    Core Nozzle method

--- nozzles.py
+++ nozzles.py
@@ -1,5 +1,19 @@
-def nozzle(T06, P06, Pa):
 
+#   Core Nozzle method. Input T06, P06, and Pa to find Te, ue, Me
+def coreNozzle(T06, P06, Pa, CpoR, Cp):
+
+    # Finds static exit temperature using isentropic relations
+    Te = T06 * (Pa/P06)^(1/CpoR)
+
+    # Find exit velocity
+    ue = sqrt(2*Cp*(T06-Te))
+
+    # Find R and gamma using CPG relations
+    R = Cp / CpoR
+    gamma = Cp / (Cp - R)
+
+    # Finds exit Mach number using above info
+    Me = ue / sqrt(gamma * R * Te)
+
+    return Te, ue, Me
     
-
-    return P07
