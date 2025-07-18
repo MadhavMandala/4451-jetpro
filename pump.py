755fbb6cb870283c35d1cc9fb820127eaa1e088e
Ian Soledade <ian.soledade@icloud.com>
2025-07-18 17:13:30 +0100

    Included efficiency inside fuel pump instead of outside

--- pump.py
+++ pump.py
@@ -1,7 +1,8 @@
-def pump(pa, pc, f, eta):
+def pump(pa, pc, f):
     rho = 780
     dpf = 20.7 * 1000
     dpinj = 572 * 1000
+    eta = 0.48
     p_in = pa + dpf
     p_out = pc + dpinj
     dp = p_out - p_in
