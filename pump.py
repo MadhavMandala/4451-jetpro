43866ff6d2e4a90a69262dd117bc09384457d403
Ian Soledade <ian.soledade@icloud.com>
2025-07-19 11:45:03 +0100

    Made turbine, fan turbine work

--- pump.py
+++ pump.py
@@ -1,10 +1,12 @@
 def pump(pa, pc, f):
     rho = 780
-    dpf = 20.7 * 1000
-    dpinj = 572 * 1000
+    dpf = 20.7
+    dpinj = 572
     eta = 0.48
     p_in = pa + dpf
     p_out = pc + dpinj
     dp = p_out - p_in
     w = dp * f / eta / rho
     return w
+
+print(pump(11, 26.96, 0.025))
--- turbine.py
+++ turbine.py
@@ -1,10 +1,9 @@
-def turbine(p_0, T_0, T_max, b, w, f):
+def turbine(p_0, T_0, b, w, f):
     C_p_over_R = lambda T_0:  3.38 + 0.700 * (T_0/1000) ** 2 - 0.200 * (T_0/1000) ** 3
     mw = 28.9
-    R = 8314 / mw
+    R = 8314.5 / mw
     C_p = C_p_over_R(T_0) * R
     gamma = C_p / (C_p - R)
-    T_max = T_max + 700 * (b/0.15) ** 0.6
     eta_p = 0.94
 
     mass_ratio = 1 + f - b
@@ -14,3 +13,23 @@ def turbine(p_0, T_0, T_max, b, w, f):
     p_0_exit = p_0 * (1 - (1 - Tr) / eta) ** (gamma / (gamma - 1))
 
     return p_0_exit, T_0_exit
+
+# print(turbine(384100, 1628, .06, 384100, 0.025))
+
+def fan_turbine(p_0, T_0, w, f):
+    C_p_over_R = lambda T_0: 3.40 + 0.630 * (T_0/1000) ** 2 - 0.200 * (T_0/1000) ** 3
+    mw = 28.9
+    R = 8314.5 / mw
+    C_p = C_p_over_R(T_0) * R
+    gamma = C_p / (C_p - R)
+    eta_p = 0.94
+
+    mass_ratio = 1 + f
+    T_0_exit = T_0 -  w/C_p/mass_ratio
+    Tr = T_0_exit / T_0
+    eta = (Tr - 1) / (Tr ** (1/eta_p) - 1)
+    p_0_exit = p_0 * (1 - (1 - Tr) / eta) ** (gamma / (gamma - 1))
+
+    return p_0_exit, T_0_exit
+
+# print(fan_turbine(146700, 1276, 40070, 0.025))
