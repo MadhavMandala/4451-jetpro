d76e5fd10e96c5ecdb524d5dbffff01c2acdd5fd
Ian Soledade <ian.soledade@icloud.com>
2025-07-20 16:45:07 +0100

    Working burner

--- boxes.py
+++ boxes.py
@@ -1,84 +1,84 @@
-import numpy as np
-#import sympy as sp
-#import matplotlib
+# import numpy as np
+# #import sympy as sp
+# #import matplotlib
 
-#T = sp.symbols('T')
+# #T = sp.symbols('T')
 
-def diffuser(ta, pa, M, n, cp_R):
-    gamma = cp_R/(cp_R-1)
-    t0a = ta * (1 + ((gamma-1)/2)*M**2)
-    p01 = pa * (1 + n*((gamma-1)/2)*M**2)**(gamma/(gamma-1))
-    t01 = t0a
-    return np.array([t01, p01])
-
-
-def compressor(t0i, p0i, n, b, cp_R, pr): 
-    gamma = cp_R/(cp_R-1)
-    p0o = p0i * pr
-    t0o = t0i * pr**((gamma-1) / (gamma * n))
-    work = (cp_R * 8314.462/28.9) * (t0o - t0i)
-    return np.array([t0o, p0o, work])
+# def diffuser(ta, pa, M, n, cp_R):
+#     gamma = cp_R/(cp_R-1)
+#     t0a = ta * (1 + ((gamma-1)/2)*M**2)
+#     p01 = pa * (1 + n*((gamma-1)/2)*M**2)**(gamma/(gamma-1))
+#     t01 = t0a
+#     return np.array([t01, p01])
 
 
-def burner(t0i, p0i, n, cp_R, f, hr, pr):
-    delH = n*f*hr
-    cp = cp_R * 8314.462/28.9
-    indefinite = sp.integrate(cp, T)
-    eq = indefinite - delH
-    solution = sp.solve(eq, T)
-    t0o = solution[0] + t0i
-    p0o = p0i * pr
-    return np.array([t0o, p0o])
+# def compressor(t0i, p0i, n, b, cp_R, pr): 
+#     gamma = cp_R/(cp_R-1)
+#     p0o = p0i * pr
+#     t0o = t0i * pr**((gamma-1) / (gamma * n))
+#     work = (cp_R * 8314.462/28.9) * (t0o - t0i)
+#     return np.array([t0o, p0o, work])
 
 
-"""
-def fuelPump():
+# def burner(t0i, p0i, n, cp_R, f, hr, pr):
+#     delH = n*f*hr
+#     cp = cp_R * 8314.462/28.9
+#     indefinite = sp.integrate(cp, T)
+#     eq = indefinite - delH
+#     solution = sp.solve(eq, T)
+#     t0o = solution[0] + t0i
+#     p0o = p0i * pr
+#     return np.array([t0o, p0o])
 
 
-
-
-def turbine():
+# """
+# def fuelPump():
 
 
 
 
-def fanTurbine():
+# def turbine():
 
 
 
 
-def bypassNozzle():
+# def fanTurbine():
 
 
 
 
-def coreNozzle():
+# def bypassNozzle():
 
 
 
 
-def combinedNozzle():
+# def coreNozzle():
 
 
 
 
-
-
-def afterburner():
-
-
-
-
-def turbineMixer():
-
-
-
-
-def nozzleMixer():
+# def combinedNozzle():
 
 
 
 
 
-"""
-print(compressor(289.2,26960,0.6,0.06,3.62,15))
+
+# def afterburner():
+
+
+
+
+# def turbineMixer():
+
+
+
+
+# def nozzleMixer():
+
+
+
+
+
+# """
+# print(compressor(289.2,26960,0.6,0.06,3.62,15))
--- burner.py
+++ burner.py
@@ -25,3 +25,32 @@ def afterburner(p_0, T_0, f, f_ab):
     # and afterburner fuel-to-air ratio.
 
 # print(afterburner(130800, 1242, 0.025, 0.005))
+
+def burner(p_0, T_0, f, b):
+    if f == 0:
+        return p_0, T_0, f
+ 
+    Pr = 0.95
+    mw = 28.9
+    R = 8314.5 / mw
+    dhr = 43.52 * 1000000
+
+    C_b = 700
+    b_max = 0.15
+    n = 0.6
+    T_max = 1400
+    T_max = T_max + C_b * (b / b_max) ** n
+    C_p = (3.70 + 0.660 * (T_0/1000) ** 2 - 0.200 * (T_0/1000) ** 3) * R
+    eta = 0.99
+
+    mass_ratio = 1 + f - b
+    p_0_exit = p_0 * Pr
+    T_0_exit = (eta*f*dhr/C_p + (1-b)*T_0) / mass_ratio
+
+    if T_0_exit > T_max:
+        T_0_exit = T_max
+        f = (T_0_exit/T_0 - 1) / ((eta*dhr/C_p/T_0) - T_0_exit/T_0)
+
+    return p_0_exit, T_0_exit, f
+
+# print(burner(404350, 657.9, 0.025, 0.06))
