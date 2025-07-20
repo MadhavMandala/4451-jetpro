431d447125bec47244172e2e869943a154f67768
Ian Soledade <ian.soledade@icloud.com>
2025-07-20 11:27:10 +0100

    Same for turbines

--- turbine.py
+++ turbine.py
@@ -1,4 +1,6 @@
 def turbine(p_0, T_0, b, w, f):
+    if w == 0:
+        return p_0, T_0
     C_p_over_R = lambda T_0:  3.38 + 0.700 * (T_0/1000) ** 2 - 0.200 * (T_0/1000) ** 3
     mw = 28.9
     R = 8314.5 / mw
@@ -17,6 +19,9 @@ def turbine(p_0, T_0, b, w, f):
 # print(turbine(384100, 1628, .06, 384100, 0.025))
 
 def fan_turbine(p_0, T_0, w, f):
+    if w == 0:
+        return p_0, T_0
+    
     C_p_over_R = lambda T_0: 3.40 + 0.630 * (T_0/1000) ** 2 - 0.200 * (T_0/1000) ** 3
     mw = 28.9
     R = 8314.5 / mw
