f10791b956b3970d2171c80661df65173aa61ab4
Ian Soledade <ian.soledade@icloud.com>
2025-07-20 10:54:09 +0100

    Fan code that's off by .1 on all values...

--- fan.py
+++ fan.py
@@ -1,2 +1,16 @@
-def fan():
-    pass
+def fan(p_0, T_0, Pr, Beta):
+    eta_p = 0.92
+    mw = 28.9
+    R = 8314.5 / mw
+    C_p = 3.5 * R
+    gamma = C_p / (C_p - R)
+
+    p_0_exit = p_0 * Pr
+    eta = (Pr ** (R/C_p) - 1) / (Pr ** (R/C_p/eta_p) - 1)
+    Tr = 1 + (Pr ** ((gamma - 1) / gamma) - 1) / eta
+    T_0_exit = T_0 * Tr
+
+    w = C_p * (T_0_exit - T_0) * (1 + Beta) / 1000
+    return p_0_exit, T_0_exit, w
+
+print(fan(22.460, 273.2, 1.2, 1.5))
