671384314edc357749b65f68d20afc122b5c636c
Ian Soledade <ian.soledade@icloud.com>
2025-07-23 03:49:41 +0100

    Ready for test case variation with top-of-file constants

--- optimize.py
+++ optimize.py
@@ -2,20 +2,25 @@ from scipy.optimize import minimize, Bounds, LinearConstraint, NonlinearConstrai
 from simulator import inputs_to_metrics
 import numpy as np
 
-# T_a, p_a, M, Pr_c, Pr_f, Beta, b, sigma, f, f_ab
+T_a = 223
+p_a = 26400
+M = 0.95
+ST = 1050
 
 def vectorized_engine(inputs):
     return inputs_to_metrics(inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5], inputs[6], inputs[7], inputs[8], inputs[9])
 
-lower_bounds = [288, 101300, 0.2,      1, 1.15, 1,    0, 0,    0, 0]
-upper_bounds = [288, 101300, 0.2, np.inf, 1.7, 15, 0.15, 1, 0.05, 0.05]
+# T_a, p_a, M, Pr_c, Pr_f, Beta, b, sigma, f, f_ab
+lower_bounds = [T_a, p_a, M,      1, 1.15, 1,    0, 0,    0, 0]
+upper_bounds = [T_a, p_a, M, np.inf, 1.7, 15, 0.15, 1, 0.05, 0.05]
 bounds = Bounds(lower_bounds, upper_bounds)
 
 Pr_const = NonlinearConstraint(lambda x: vectorized_engine(x)["Pr_c"] * vectorized_engine(x)["Pr_f"], 0, 55)
 Tb_const = NonlinearConstraint(lambda x: vectorized_engine(x)["T_04"] - vectorized_engine(x)["T_max"], -np.inf, 0)
 Tb_ab_const = NonlinearConstraint(lambda x: vectorized_engine(x)["T_06"] - vectorized_engine(x)["T_max_ab"], -np.inf, 0)
-ST_const = NonlinearConstraint(lambda x: vectorized_engine(x)["ST"], 1230, 1230)
+ST_const = NonlinearConstraint(lambda x: vectorized_engine(x)["ST"], ST, ST)
 
+# T_a, p_a, M, Pr_c, Pr_f, Beta, b, sigma, f, f_ab
 x0 = np.array([288, 101300, 0.2, 10.0, 1.2, 0.03, 0.0, 0.06, 2, 0.5])
 res = minimize(
     lambda x: vectorized_engine(x)["TSFC"],
@@ -23,11 +28,11 @@ res = minimize(
     method="SLSQP",
     bounds=bounds,
     constraints=[Pr_const, Tb_const, Tb_ab_const, ST_const],
-    options=dict(maxiter=2000, ftol=1e-9)
+    options=dict(maxiter=10000, ftol=1e-15)
 )
 print(res.message)
 outputs = vectorized_engine(res.x)
-print("TSFC:", outputs["TSFC"]*3600*1000)  # Convert to kg/kN/h`)
+print("TSFC:", outputs["TSFC"]*3600*1000)  # Convert to kg/kN/h
 print("Specific Thrust:", outputs["ST"]/1000)
 print("Overall Efficiency:", outputs["eta_o"]*100)
 # print("T_a, p_a, M, Pr_c, Pr_f, Beta, b, sigma, f, f_ab:\n", res.x)
